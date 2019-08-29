efdc368a27d470aa5b40cae8a99e9f01d4af5cfe
redfox0927 <redfox0927@gmail.com>
2019-08-29 18:06:21 +0900

    commit css

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Iframe from 'react-iframe'
 import { userService, authenticationService } from '@/_services';
 import ReactWeather from 'react-open-weather';
 import 'react-open-weather/lib/css/ReactWeather.css';
@@ -22,16 +22,27 @@ class HomePage extends React.Component {
         const { currentUser, users } = this.state;
         return (
             <div>
-                <h1>Hi {currentUser.username}!</h1>
-                <div style={{ marginTop: '50px' }}>  
-                    <ReactWeather
-                        forecast="5days"
-                        apikey="10bc6d391be8422b803232333192005"
-                        type="city"
-                        city={currentUser.city}
-                        lang="ko"
-                    />
-        </div><br />
+                <div>
+                    <h1>Hi {currentUser.username}!</h1>
+                    <div style={{ marginTop: '50px' }}>  
+                        <ReactWeather
+                            forecast="5days"
+                            apikey="10bc6d391be8422b803232333192005"
+                            type="city"
+                            city={currentUser.city}
+                            lang="ko"
+                        />
+                </div><br />
+                </div>
+                <div>
+                <Iframe url="https://34.67.187.63"
+                        width="540px"
+                        height="311px"
+                        id="myId"
+                        className="myClassname"
+                        display="initial"
+                        position="relative"/>
+                </div>
             </div>
         );
     }
